4ba080eb3f9d632508d3b2dd5b19b827d24f7038
Geoffrey Cestaro <gcint2@outlook.com>
2017-09-24 10:58:49 +0000

    Integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,8 +1,7 @@
 class Portfolio < ApplicationRecord
+    has_many :technologies
     include Placeholder
-    
     validates_presence_of :title, :body, :main_image, :thumb_image
-    
     after_initialize :set_defaults
     
     def set_defaults
