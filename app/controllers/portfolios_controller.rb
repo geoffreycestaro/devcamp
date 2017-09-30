7285bf6e05416dd91360455715f30aa41e992b92
Geoffrey Cestaro <gcint2@outlook.com>
2017-09-30 15:17:00 +0000

    Implemented partials with local data for portfolio form and nav bars.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,65 +1,73 @@
 class PortfoliosController < ApplicationController
-
   layout 'portfolio'
 
-    def index
-      @portfolio_items = Portfolio.all
-    end
+  def index
+    @portfolio_items = Portfolio.all
+  end
 
-    def new
-      @portfolio_item = Portfolio.new
-      3.times { @portfolio_item.technologies.build }
-    end
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
 
-    # POST /portfolio
-    def create
-      @portfolio_item = Portfolio.new(portfolio_params)
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
 
-      respond_to do |format|
-        if @portfolio_item.save
-          format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
-        else
-          format.html { render :new }
-        end
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
       end
     end
+  end
 
-    def edit
-      @portfolio_item = Portfolio.find(params[:id])
-    end
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
 
-    # PATCH/PUT /portfolios/1
-    def update
-      @portfolio_item = Portfolio.find(params[:id])
-      respond_to do |format|
-        if @portfolio_item.update(portfolio.params)
-        else
-          format.html { render :edit }
-        end
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
+  end
 
-    def show
-       @portfolio_item = Portfolio.find(params[:id])
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
+  end
 
-    # Set portfolio item, destroy, redirect.
-    def destroy
-      @portfolio_item = Portfolio.find(params[:id])
-      @portfolio_item.destroy
-      respond_to do |format|
-        format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully destroyed.' }
-      end
-    end
+  private
 
-    private
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def portfolio_params
-      params.require(:portfolio).permit(:title,
-                                        :subtitle,
-                                        :body,
-                                        technologies_attributes: [:name])
-    end
 end
-
