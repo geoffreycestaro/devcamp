071c411fae60ea780d3fe9982f16f4a75fee89ae
Geoffrey Cestaro <gcint2@outlook.com>
2017-10-01 13:06:23 +0000

    Implemented bootstrap.

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,22 +28,22 @@ puts "5 skills created"
     Portfolio.create!(
         title: "Portfolio title: #{portfolio_item}",
         subtitle: "Ruby on Rails",
-        body: "James is the founder of Shopify Expert agency OnlyGrowth, which helps Shopify merchants with conversion optimization and email marketing. James also runs Agency Blueprint, a mentorship program for growing Shopify Partners and ecommerce agencies.",
+        body: "Simon is a coffee lover, former agency digital strategist, and Content Marketing Lead. When he isn’t hustling at HQ, you can most likely find him dining at restaurants across the city or brushing up on the latest design trends. Nick is a developer, tech enthusiast, and UX lover. He's spent the last 10 years working in the software industry, with a specialized focus on development. He counts advertising, psychology, and cinema among his myriad interests.",
         main_image: "http://via.placeholder.com/600x400",
         thumb_image: "http://via.placeholder.com/350x200"
         )
     end
-    
+
 1.times do |portfolio_item|
 Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "Angular",
-    body: "James is the founder of Shopify Expert agency OnlyGrowth, which helps Shopify merchants with conversion optimization and email marketing. James also runs Agency Blueprint, a mentorship program for growing Shopify Partners and ecommerce agencies.",
+    body: "Simon is a coffee lover, former agency digital strategist, and Content Marketing Lead. When he isn’t hustling at HQ, you can most likely find him dining at restaurants across the city or brushing up on the latest design trends. Nick is a developer, tech enthusiast, and UX lover. He's spent the last 10 years working in the software industry, with a specialized focus on development. He counts advertising, psychology, and cinema among his myriad interests.",
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/350x200"
     )
 end
-    
+
 puts "9 portfolio items created"
 
 3.times do |technology|
@@ -51,5 +51,5 @@ puts "9 portfolio items created"
             name: "Technology #{technology}"
         )
 end
-    
+
 puts "3 technology items created"
