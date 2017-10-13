b9ed6f239db43b157e734c685040893c04436737
Geoffrey Cestaro <gcint2@outlook.com>
2017-10-13 11:51:53 +0000

    Finished implementing action cable.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog
 
-  validates :content, presence: true, length: { minimum: 5, maximum: 500 }
+  validates :content, presence: true, length: { minimum: 5, maximimum: 500 }
 
   after_create_commit { CommentBroadcastJob.perform_later(self) }
 end
